3f559c32236381553bdb4f274b0aaa11ca4f3c9c
MegaEmme <mikmusco@gmail.com>
2025-04-14 15:36:13 +0200

    feat: working on controlled elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false
+  })
+
+  function handleFormData(element) {
+    setFormData((formData) => ({
+      ...formData,
+      [element.target.name]: element.target.value,
+    }))
+  }
+
   return (
     <>
-
+      <h1>React Post Form</h1>
+      <label htmlFor="">Inserisci autore</label>
+      <input type="text"
+        name="author"
+        value={formData.author}
+        onChange={handleFormData}
+        placeholder='Inserisci autore' />
+      <hr />
+      <label htmlFor="">Inserisci titolo post</label>
+      <input type="text"
+        name="title"
+        value={formData.title}
+        onChange={handleFormData}
+        placeholder='Inserisci titolo' />
+      <hr />
+      <label htmlFor="">Inserisci testo del post</label>
+      <input type="text"
+        name="body"
+        value={formData.body}
+        onChange={handleFormData}
+        placeholder='Inserisci contenuto' />
+      {console.log(formData)}
     </>
   )
 }
