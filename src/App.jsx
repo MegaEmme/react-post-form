bbbf8395c4f62774c11c8c0dbfc6127c0e33b39d
MegaEmme <mikmusco@gmail.com>
2025-04-14 16:26:30 +0200

    feat: done exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,83 @@
 import { useState } from 'react';
 import axios from 'axios';
 
+const initialFormData = {
+  author: "",
+  title: "",
+  body: "",
+  public: false
+};
+
 function App() {
 
-  const [formData, setFormData] = useState({
-    author: "",
-    title: "",
-    body: "",
-    public: false
-  })
+  const [formData, setFormData] = useState(initialFormData);
+
+  const endpoint = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
+
+  function fetchPost() {
+    axios.post(endpoint, formData)
+      .then(res => {
+        setFormData(res.data)
+      })
+      .catch(err => console.log(err))
+  }
 
   function handleFormData(element) {
+    const value =
+      element.target.type === "checkbox" ?
+        element.target.checked : element.target.value;
+
     setFormData((formData) => ({
       ...formData,
-      [element.target.name]: element.target.value,
-    }))
+      [element.target.name]: value,
+    }));
+  };
+
+  function handleSubmit(element) {
+    element.preventDefault();
+    fetchPost();
+    setFormData(initialFormData);
   }
 
   return (
     <>
       <h1>React Post Form</h1>
-      <label htmlFor="">Inserisci autore</label>
-      <input type="text"
-        name="author"
-        value={formData.author}
-        onChange={handleFormData}
-        placeholder='Inserisci autore' />
-      <hr />
-      <label htmlFor="">Inserisci titolo post</label>
-      <input type="text"
-        name="title"
-        value={formData.title}
-        onChange={handleFormData}
-        placeholder='Inserisci titolo' />
-      <hr />
-      <label htmlFor="">Inserisci testo del post</label>
-      <input type="text"
-        name="body"
-        value={formData.body}
-        onChange={handleFormData}
-        placeholder='Inserisci contenuto' />
-      {console.log(formData)}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="author">Inserisci autore</label>
+        <br />
+        <input type="text"
+          name="author"
+          value={formData.author}
+          onChange={handleFormData}
+          placeholder='Inserisci autore' />
+        <hr />
+        <label htmlFor="title">Inserisci titolo post</label>
+        <br />
+        <input type="text"
+          name="title"
+          value={formData.title}
+          onChange={handleFormData}
+          placeholder='Inserisci titolo' />
+        <hr />
+        <label htmlFor="body">Inserisci testo del post</label>
+        <br />
+        <input type="text"
+          name="body"
+          value={formData.body}
+          onChange={handleFormData}
+          placeholder='Inserisci contenuto' />
+        <hr />
+        <label htmlFor="public">Post privato</label>
+        <br />
+        <input type="checkbox"
+          name="public"
+          checked={formData.public}
+          onChange={handleFormData}
+          id="public" />
+        <hr />
+        <button>Invio</button>
+        {console.log(formData)}
+      </form>
     </>
   )
 }
